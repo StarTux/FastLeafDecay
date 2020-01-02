593d3a98bdba7c66918def9334c19bd6c2a0ccc3
StarTux <ths.loy@googlemail.com>
2020-01-02 15:49:10 +0100

    Update coding style; Spigot 1.15.1; remove plugin annotations

--- src/main/java/com/cavetale/fastleafdecay/FastLeafDecayPlugin.java
+++ src/main/java/com/cavetale/fastleafdecay/FastLeafDecayPlugin.java
@@ -15,24 +15,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Description;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-import org.bukkit.plugin.java.annotation.plugin.Website;
-import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
-@Plugin(name = "FastLeafDecay", version = "1.0")
-@Description("Speed up Leaf Decay")
-@ApiVersion(ApiVersion.Target.v1_13)
-@Author("StarTux")
-@Website("https://cavetale.com")
 public final class FastLeafDecayPlugin extends JavaPlugin implements Listener {
     private final Set<String> onlyInWorlds = new HashSet<>();
     private final Set<String> excludeWorlds = new HashSet<>();
-    private long breakDelay, decayDelay;
-    private boolean spawnParticles, playSound;
+    private long breakDelay;
+    private long decayDelay;
+    private boolean spawnParticles;
+    private boolean playSound;
     private final Set<Block> scheduledBlocks = new HashSet<>();
-    private static final BlockFace[] NEIGHBORS = {BlockFace.UP, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.DOWN};
+    private static final BlockFace[] NEIGHBORS = {
+        BlockFace.UP,
+        BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST,
+        BlockFace.DOWN
+    };
 
     @Override
     public void onEnable() {
@@ -87,14 +83,17 @@ public final class FastLeafDecayPlugin extends JavaPlugin implements Listener {
      * @param delay the delay of the scheduled check, in ticks
      */
     private void onBlockRemove(final Block oldBlock, long delay) {
-        if (!Tag.LOGS.isTagged(oldBlock.getType()) && !Tag.LEAVES.isTagged(oldBlock.getType())) return;
+        if (!Tag.LOGS.isTagged(oldBlock.getType())
+            && !Tag.LEAVES.isTagged(oldBlock.getType())) {
+            return;
+        }
         final String worldName = oldBlock.getWorld().getName();
         if (!onlyInWorlds.isEmpty() && !onlyInWorlds.contains(worldName)) return;
         if (excludeWorlds.contains(worldName)) return;
         for (BlockFace neighborFace: NEIGHBORS) {
             final Block block = oldBlock.getRelative(neighborFace);
             if (!Tag.LEAVES.isTagged(block.getType())) continue;
-            Leaves leaves = (Leaves)block.getBlockData();
+            Leaves leaves = (Leaves) block.getBlockData();
             if (leaves.isPersistent()) continue;
             if (scheduledBlocks.contains(block)) continue;
             scheduledBlocks.add(block);
@@ -117,17 +116,23 @@ public final class FastLeafDecayPlugin extends JavaPlugin implements Listener {
     private void decay(Block block) {
         if (!scheduledBlocks.remove(block)) return;
         if (!Tag.LEAVES.isTagged(block.getType())) return;
-        Leaves leaves = (Leaves)block.getBlockData();
+        Leaves leaves = (Leaves) block.getBlockData();
         if (leaves.isPersistent()) return;
         if (leaves.getDistance() < 7) return;
         LeavesDecayEvent event = new LeavesDecayEvent(block);
         getServer().getPluginManager().callEvent(event);
         if (event.isCancelled()) return;
         if (spawnParticles) {
-            block.getWorld().spawnParticle(Particle.BLOCK_DUST, block.getLocation().add(0.5, 0.5, 0.5), 8, 0.2, 0.2, 0.2, 0, block.getType().createBlockData());
+            block.getWorld()
+                .spawnParticle(Particle.BLOCK_DUST,
+                               block.getLocation().add(0.5, 0.5, 0.5),
+                               8, 0.2, 0.2, 0.2, 0,
+                               block.getType().createBlockData());
         }
         if (playSound) {
-            block.getWorld().playSound(block.getLocation(), Sound.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 0.05f, 1.2f);
+            block.getWorld().playSound(block.getLocation(),
+                                       Sound.BLOCK_GRASS_BREAK,
+                                       SoundCategory.BLOCKS, 0.05f, 1.2f);
         }
         block.breakNaturally();
     }
